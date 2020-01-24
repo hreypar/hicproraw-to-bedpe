7a5817392399ce9348feca87f8d29bbd37bfaff8
hreypar <hreypar@gmail.com>
2020-01-24 16:35:32 -0600

    create R script

---
+++ bin/generate-hicexp.R
@@ -0,0 +1,9 @@
+###################################################################
+# hreyes Jan 2020
+#
+# generate-hicexp.R
+#
+# Read in a raw matrix (output from HiC-Pro) and format it to
+# generate a hicexp object
+###################################################################
+
