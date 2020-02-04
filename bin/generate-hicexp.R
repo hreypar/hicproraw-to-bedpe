425357f99ebdd1cd7bf8210d7741619b190aaed8
hreypar <hreypar@gmail.com>
2020-02-03 18:48:55 -0600

    correct script desc

--- bin/generate-hicexp.R
+++ bin/generate-hicexp.R
@@ -3,7 +3,7 @@
 # hreyes Jan 2020
 # generate-hicexp.R
 #
-# Read in a raw matrix (output from HiC-Pro) and format it to generate a hicexp object
+# Read in a raw matrix (output from HiC-Pro) and format it to generate a bedpe object
 #
 #
 #################### import libraries and set options ####################
